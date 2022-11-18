5f3187d5c218d41261a85280c644c71db7e1b79e
chiacn <chiacn@naver.com>
2022-11-18 20:12:49 +0900

    router.js 의 createBrowserRouter 배열 버전

--- src/router/router.js
+++ src/router/router.js
@@ -15,47 +15,46 @@ import ProtectedRoutes from "./protectedRoutes";
 
 export const WrappingRouter = (props) => {
 
-  console.log('WrappingRouter 작동 =======================')
+  //createBrowserRouter 배열 버전 구조
+  const router = createBrowserRouter([
+    {
+      element: <ProtectedRoutes/>,
+      children: [
+        {
+          path: "/",
+          element: <Navigate to="/main"/>, // <------------여기다 App을 하면 안 됨. ..  ★ App 컴포넌트(최상위) 내에서★ 렌더링 하는것이므로.
+        },
+        {
+          path: "main",
+          element: <Main />
+        },
+        {
+          path: "posts",
+          element: 
+            <div> This is Parent Component Example for Outlet Component
+              <Outlet/>
+            </div>,
+          children: [
+              {
+                path: ":id",
+                element: <Posts/>
+              },
+              {
+                path: "1",
+                element: <Posts/>
+              }
+          ] 
+        }
+      ]
+    },
+    {
+      path:"/login",
+      element: <Login/>
+    }
 
-
-/*
-    createBrowserRouter를 사용하는 것보다 
-    그냥 <Route>형태로 하는게 Auth체크 중 useLocation도 사용할 수 있고 여러모로 좋은듯..?
- */
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: Auth(<Main />), // <------------여기다 App을 하면 안 됨. ..  ★ App 컴포넌트(최상위) 내에서★ 렌더링 하는것이므로.
-  //     children: [
-  //       {
-  //         path: "maker",
-  //         element: <Maker />
-  //       },
-  //     ]
-  //   },
-  //   {
-  //     path:"login",
-  //     element: <Login authService={authService}/>
-  //   }
-
-  // ]
-  // );
-  // const routerProvider= <RouterProvider router={router}/>
-
-  // function Auth(Component, authCheck=false, ...rest) {
-  //   if(!authCheck) {
-  //     return (
-  //       (userInfo.userInfo !== null) ?  
-  //       Component :
-  //       <Navigate to="/login" replace={false} /> // replace : history에 이력을 남길 것인가 아닌가.
-  //       )
-          
-  //   }else {
-  //     // 유저별 권한체크
-      
-  //   }
-  // }
-
+  ]
+  );
+  const routerProvider= <RouterProvider router={router}/>
 
 
 /*
@@ -64,40 +63,46 @@ export const WrappingRouter = (props) => {
     Auth체크 중 useLocation도 사용할 수 있고 여러모로 좋은듯..?
 
  */
-  const router = createBrowserRouter(
-    createRoutesFromElements(
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
  
-        // <Route>
-        //   <Route path="/" element={Auth(<Main/>)} />
-        //   <Route path="maker" element={<Maker/>}/> 
-        //   <Route path="login" element={<Login/>}/>
-        // </Route>
+  //       // <Route>
+  //       //   <Route path="/" element={Auth(<Main/>)} />
+  //       //   <Route path="maker" element={<Maker/>}/> 
+  //       //   <Route path="login" element={<Login/>}/>
+  //       // </Route>
         
-        <Route >
-          <Route element={<ProtectedRoutes/>}>
-              <Route path="/" element={<Navigate to="/main"/>}/>
-              <Route path="main" element={<Main/>}/>
+  //       <Route >
+  //         <Route element={<ProtectedRoutes/>}>
+  //             <Route path="/" element={<Navigate to="/main"/>}/>
+  //             <Route path="main" element={<Main/>}/>
 
-              <Route path="maker" element={<Maker/>}/>
-              <Route path="posts" element={
-                  <div> This is Parent Component Example for Outlet Component 
-                    <Outlet/> 
-                  </div>
-                }>
+  //             <Route path="maker" element={<Maker/>}/>
+  //             <Route path="posts" element={
+  //                 <div> This is Parent Component Example for Outlet Component 
+  //                   <Outlet/> 
+  //                 </div>
+  //               }>
               
-                  <Route path=":id" element={<Posts/>}/>
-                  <Route path="1" element={<Posts/>}/>
-              </Route>
-          </Route>
-          <Route path="*" element={<NotFound/>}/>
-          <Route path="login" element={<Login/>}/>
+  //                 <Route path=":id" element={<Posts/>}/>
+  //                 <Route path="1" element={<Posts/>}/>
+  //             </Route>
+  //         </Route>
+  //         <Route path="*" element={<NotFound/>}/>
+  //         <Route path="login" element={<Login/>}/>
             
-        </Route>
-    )
-  );
-  const routerProvider= <RouterProvider router={router}/>
+  //       </Route>
+  //   )
+  // );
+  // const routerProvider= <RouterProvider router={router}/>
+
+
 
 
 
   return routerProvider
 }
+
+
+
+
