70470585d93df2cc9ac1a6624e885df576036bfb
chiacn <chiacn@naver.com>
2023-01-25 16:58:02 +0900

    Add react-helmet, react-snap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
-
+import { Helmet } from 'react-helmet-async';
 
 function App() {
 
   return (
       <>
+        <Helmet>
+          <title>Save Your Time</title>
+          <meta name="title" content="Save Your Time - simple timer web"/>
+          <meta name="description" content ="You can self study efficiently by using timer web. Let's manage your learning time!"/>
+          <meta name="keyword" content="timer, motivation, study, learn"/>
+        </Helmet>
         <Navbar/>
         <Outlet/>
       </>
